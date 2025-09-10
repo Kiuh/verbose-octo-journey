5a44214b8f111cdf80927c26a317c74303fe2176
Ivan Shkel <skillofivan@gmail.com>
2025-09-10 11:50:07 +0300

    [VOJ-4] Fix linter errors
    Rules used:
    - `function_naming`
    - `no_unused`
    - `no_deprecated`
    - `field_naming`
    - `declaration_naming`
    - `no_comment_out_code`

--- projects/raylib-snake/src/snake/snake.zig
+++ projects/raylib-snake/src/snake/snake.zig
@@ -12,7 +12,7 @@ const Segment = @import("segment.zig").Segment;
 pub const Snake = struct {
     segments: ArrayList(Segment) = undefined,
 
-    last_movement_dir: Direction = .down,
+    last_movement_dir: Direction = .downward,
     input_direction: Direction = .none,
 
     // I'd leave this a reference if we want to read and update it on-the-fly in the future
@@ -29,13 +29,13 @@ pub const Snake = struct {
         self.segments.deinit(allocator);
     }
 
-    pub fn get_head(self: *Snake) Segment {
+    pub fn getHead(self: *Snake) Segment {
         return self.segments.items[0];
     }
 
     pub fn restart(self: *Snake, allocator: Allocator) !void {
         self.segments.clearAndFree(allocator);
-        self.last_movement_dir = .down;
+        self.last_movement_dir = .downward;
         self.input_direction = .none;
 
         for (0..self.gcfg.init_snake_length) |i| {
@@ -53,7 +53,7 @@ pub const Snake = struct {
         try self.segments.append(allocator, seg);
     }
 
-    pub fn check_overlap(self: *Snake) bool {
+    pub fn checkOverlap(self: *Snake) bool {
         const head = self.segments.items[0];
         for (self.segments.items) |seg| {
             if (Vec2.isEqual(seg.pos, head.pos)) {
@@ -73,7 +73,7 @@ pub const Snake = struct {
                 if (self.input_direction != .none) {
                     self.last_movement_dir = self.input_direction;
                 }
-                self.segments.items[i].pos = Vec2.add(segment.pos, utils.direction_to_vector(self.last_movement_dir));
+                self.segments.items[i].pos = Vec2.add(segment.pos, utils.directionToVector(self.last_movement_dir));
                 self.input_direction = .none;
                 pos_to_set = prev_pos;
                 continue;
